b13198fcee0b8c033d7d0320c687d8764d92fc3a
ashen iranga hewarathna <asheniranga@gmail.com>
2022-03-05 19:57:25 +0530

    baseline training results patch-1

--- preprocessing/image/basic.py
+++ preprocessing/image/basic.py
@@ -13,14 +13,18 @@ class BasicAugmentation:
         return
 
     @staticmethod
-    def random_horizontal_flip(path, dst):
+    def flip(path, dst):
         images = os.listdir(path)
 
         for image in images:
             image_path = os.path.join(path, image)
-            dst_path = os.path.join(dst, image)
+            dst_lr_path = os.path.join(dst, f'_flip_l_r_{image}')
+            dst_ud_path = os.path.join(dst, f'_flip_u_d_{image}')
             img = PIL.Image.open(image_path)
-            flip = tf.image.flip_left_right(img)
+            flip_lr = array_to_img(tf.image.flip_left_right(img_to_array(img)))
+            flip_ud = array_to_img(tf.image.flip_up_down(img_to_array(img)))
 
             # save flipped image in destination
-            flip.save(dst_path)
+            flip_lr.save(dst_lr_path)
+            flip_ud.save(dst_ud_path)
+            img.save(os.path.join(dst, image))
