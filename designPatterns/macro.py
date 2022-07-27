05e64ac013ae10b68e35c079aedcb3a7e2240f75
ashen iranga hewarathna <asheniranga@gmail.com>
2022-07-27 22:31:44 +0530

    macro architecture for cnn's

---
+++ designPatterns/macro.py
@@ -0,0 +1,26 @@
+from tensorflow.keras.models import Model
+
+
+def steam(input_shape):
+    """ steam layers
+    :param input_shape: the shape of the input tensor
+    :return:
+    """
+    pass
+
+
+def learner(inputs):
+    """ learner layers
+    :param inputs: the input tensor (feature maps)
+    :return:
+    """
+    pass
+
+
+def task(inputs, n_classes):
+    """ classifier layers
+    :param inputs: input tensor
+    :param n_classes: number of output classes
+    :return:
+    """
+    pass
